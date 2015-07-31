74720f73625316b58a561e9d1faf493968e9bf97
Carla Wilby <thisiscarlawilby@gmail.com>
2015-07-31 19:48:24 +0200

    Eating cats, whoo!

---
+++ Catgario.js
@@ -0,0 +1,9 @@
+/**
+ * Created by user on 2015-07-31.
+ */
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
