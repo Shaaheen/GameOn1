6ee9f6c50eaa7d9af236a22c8f0ffa1e56b3f3d8
Carla Wilby <thisiscarlawilby@gmail.com>
2015-07-31 22:23:43 +0200

    Black background

--- Catgario.js
+++ Catgario.js
@@ -5,9 +5,9 @@ var game;
 
 game = new Phaser.Game(600, 450, Phaser.AUTO, '');
 
-game.state.add('Menu', Menu);
+//game.state.add('Menu', Menu);
 game.state.add('Game', Game);
 game.state.add('Game_Over', Game_Over);
+game.state.start('Game');
 
-
-game.state.start('Menu');
+//game.state.start('Menu');
